d142c3010c869bfeda7f59726b7c10272851024f
KIMOTO Mizuki <teddy@sfc.wide.ad.jp>
2015-02-27 17:50:43 +0900

    added command line arg

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "X$1" = "X" ]; then
-	echo "Usage: $0 dstaddr"
+if [ "X$2" = "X" ]; then
+	echo "Usage: $0 dst_ip tunnle_ip"
 	exit -1
 fi
 
@@ -9,4 +9,4 @@ sudo insmod wtun.ko dst_addr=$1
 
 sudo iwconfig wlan0 mode ad-hoc
 sudo iwconfig wlan0 essid test
-sudo ifconfig wlan0 up
+sudo ifconfig wlan0 $2/24
