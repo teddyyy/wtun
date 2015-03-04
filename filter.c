0d25cba8cfd1bd269838096a4d59d80635eb7baf
KIMOTO Mizuki <teddy@sfc.wide.ad.jp>
2015-03-05 00:17:25 +0900

    typo

--- filter.c
+++ filter.c
@@ -40,6 +40,8 @@ static unsigned int wtun_hook_funk(unsigned int hooknum,
                        				const struct net_device *out,
                        				int (*okfn)(struct sk_buff*))
 {
+	pr_info("%s\n", __func__);
+
 	if (is_tunnel_data(skb)) {
 		int iphlen, rest_wtun_header_len;
 		struct ieee80211_rx_status stat;
