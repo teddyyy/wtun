8b606a10d39bc309e70fe97afd8257f23594aec5
teddy <teddy@sfc.wide.ad.jp>
2017-04-05 23:38:35 +0900

    fix indent 2

--- dev.h
+++ dev.h
@@ -14,33 +14,33 @@ struct wtun_hw* get_wtun_dev(void);
 
 static struct ieee80211_channel wtun_channel[] = {
 	{
-    	.band = IEEE80211_BAND_2GHZ,
-        .center_freq = 2412,
-        .hw_value = 2412,
-        .max_power = 25,
-    },
-    {
-    	.band = IEEE80211_BAND_2GHZ,
-        .center_freq = 2437,
-        .hw_value = 2437,
-        .max_power = 25,
-    },
-   	{
-    	.band = IEEE80211_BAND_2GHZ,
-        .center_freq = 2462,
-        .hw_value = 2462,
-        .max_power = 25,
-    },
+		.band = IEEE80211_BAND_2GHZ,
+		.center_freq = 2412,
+		.hw_value = 2412,
+		.max_power = 25,
+	},
+	{
+		.band = IEEE80211_BAND_2GHZ,
+		.center_freq = 2437,
+		.hw_value = 2437,
+		.max_power = 25,
+	},
+	{
+		.band = IEEE80211_BAND_2GHZ,
+		.center_freq = 2462,
+		.hw_value = 2462,
+		.max_power = 25,
+	},
 };
 
 static const struct ieee80211_rate wtun_rate[] = {
 	{ .bitrate = 10,  .flags = 0 },
-   	{ .bitrate = 20,  .flags = IEEE80211_RATE_SHORT_PREAMBLE },
-    { .bitrate = 55,  .flags = IEEE80211_RATE_SHORT_PREAMBLE },
-    { .bitrate = 110, .flags = IEEE80211_RATE_SHORT_PREAMBLE },
-    { .bitrate = 60,  .flags = 0 },
-    { .bitrate = 120, .flags = 0 },
-    { .bitrate = 240, .flags = 0 },
+	{ .bitrate = 20,  .flags = IEEE80211_RATE_SHORT_PREAMBLE },
+	{ .bitrate = 55,  .flags = IEEE80211_RATE_SHORT_PREAMBLE },
+	{ .bitrate = 110, .flags = IEEE80211_RATE_SHORT_PREAMBLE },
+	{ .bitrate = 60,  .flags = 0 },
+	{ .bitrate = 120, .flags = 0 },
+	{ .bitrate = 240, .flags = 0 },
 };
 
 struct station_data {
